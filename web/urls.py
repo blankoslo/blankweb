657b1fb4df061a0869892076405b55718d464840
Magne Davidsen <magne.davidsen@gmail.com>
2016-03-04 16:16:56 +0100

    started on postings

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    url(r'^jobb/(?P<posting_id>[\w-]+)', views.posting, name='posting'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
 
 from .models import Employee
+
+POSTINGS_ROOT = 'https://api.lever.co/v0/postings/blankoslo';
 
 def index(request):
     all_employees = Employee.objects.all()
     context = {'all_employees': all_employees}
     return render(request, 'web/index.html', context)
+
+def posting(request, posting_id):
+    r = requests.get(POSTINGS_ROOT + '/' + posting_id)
+    return render(request, 'web/posting.html', r.json())
