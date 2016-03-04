d4d9071eb06811b848384a8ece791f86e5b99a2d
Magne Davidsen <magne.davidsen@gmail.com>
2016-03-04 12:18:36 +0100

    templating on fleek

--- web/models.py
+++ web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=256)
+    email = models.EmailField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
