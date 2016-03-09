6181698930b1cc7d351c6a2417dc08fae44b340e
Magne Davidsen <magne.davidsen@gmail.com>
2016-03-09 13:47:42 +0100

    single job postings

--- web/views.py
+++ web/views.py
@@ -4,7 +4,11 @@ import requests
 
 from .models import Employee
 
-POSTINGS_ROOT = 'https://api.lever.co/v0/postings/blankoslo';
+POSTINGS_ROOT = 'https://api.lever.co/v0/postings/blankoslo'
+
+TECH_TEAM = 'Teknologi'
+DESIGN_TEAM = 'Brukeropplevelse'
+BUSINESS_TEAM = 'Forretningsdesign'
 
 def index(request):
     all_employees = Employee.objects.all()
@@ -12,5 +16,15 @@ def index(request):
     return render(request, 'web/index.html', context)
 
 def posting(request, posting_id):
-    r = requests.get(POSTINGS_ROOT + '/' + posting_id)
-    return render(request, 'web/posting.html', r.json())
+    def get_template(team):
+        return {
+            TECH_TEAM : 'web/posting_tech.html',
+            DESIGN_TEAM : 'web/posting_design.html',
+            BUSINESS_TEAM : 'web/posting_business.html',
+        }.get(team, 'web/posting_business.html')
+
+    posting_dict = requests.get(POSTINGS_ROOT + '/' + posting_id).json()
+    template = get_template(posting_dict['categories']['team'])
+    context = {'text': posting_dict['text'], 'description': posting_dict['description']}
+
+    return render(request, template, context)
