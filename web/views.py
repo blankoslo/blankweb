2b0ff7229b80e0d792839077fe3a893c7b565099
Magne Davidsen <magne.davidsen@gmail.com>
2016-09-19 13:42:01 +0200

    Add correct bg colors on postings page

--- web/views.py
+++ web/views.py
@@ -13,13 +13,11 @@ def index(request):
 
 def posting(request, posting_id):
     posting_dict = requests.get(POSTINGS_ROOT + '/' + posting_id).json()
-    print (posting_dict)
     team = posting_dict['categories']['team'].lower()
     context = {'posting':posting_dict, 'category': JobPostingCategory.objects.filter(team=team)[0]}
-    print (context)
     return render(request, 'web/posting.html', context)
 
 def team_postings(request, team):
     postings_array = requests.get(POSTINGS_ROOT + '?team=' + team + '&mode=json').json()
-    context = {'team_postings' :  postings_array, 'category': JobPostingCategory.objects.filter(team=team)[0]}
+    context = {'team_postings' :  postings_array, 'category': JobPostingCategory.objects.filter(team=team)[0], 'team' : team}
     return render(request, 'web/postings.html', context)
