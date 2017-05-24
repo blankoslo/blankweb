c30161c8da0e168a1a132b84db139c566b31462c
Magne Davidsen <magne.davidsen@gmail.com>
2017-05-24 12:30:05 +0200

    Replace teknologi posting with custom one

--- web/urls.py
+++ web/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^jobb/stilling/(?P<posting_id>[\w-]+)', views.posting, name='posting'),
     url(r'^jobb/brukeropplevelse', views.team_design, name='team_design'),
+    url(r'^jobb/teknologi', views.team_teknologi, name='team_teknologi'),
     url(r'^jobb/(?P<team>[\w]+)', views.team_postings, name='team_postings'),
 
 ]
